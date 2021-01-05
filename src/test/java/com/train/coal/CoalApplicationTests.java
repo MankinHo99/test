709ffaec63d38cb3bae73f7a94d4834605a35cd6
MankinHo99 <jimsheminjian@gmail.com>
2021-01-05 14:40:09 +0800

    git使用演示

---
+++ src/test/java/com/train/coal/CoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.train.coal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

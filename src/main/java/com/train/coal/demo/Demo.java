374828faeda773b479b986cfea2195d0fa7db407
MankinHo99 <jimsheminjian@gmail.com>
2021-01-07 16:45:13 +0800

    完成注册功能

--- src/main/java/com/train/coal/demo/Demo.java
+++ src/main/java/com/train/coal/demo/Demo.java
@@ -1,7 +1,14 @@
 package com.train.coal.demo;
 
+
+import org.springframework.util.DigestUtils;
+
+import java.nio.charset.StandardCharsets;
+
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("hehe");
+        String i="11111";
+        String pwd= DigestUtils.md5DigestAsHex(i.getBytes(StandardCharsets.UTF_8));
+        System.out.println(pwd);
     }
 }
